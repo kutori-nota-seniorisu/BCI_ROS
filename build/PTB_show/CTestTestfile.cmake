13f656d3fcb9bc457f65649136b14d096c1a3a06
wuyou <1443207784@qq.com>
2023-06-14 13:07:11 +0800

    测试了cv_bridge 在20.04上没有问题

--- build/PTB_show/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wuyou/BCI_ROS/src/PTB_show
-# Build directory: /home/wuyou/BCI_ROS/build/PTB_show
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/online_analysis/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/wuyou/BCI_ROS/src/online_analysis
-# Build directory: /home/wuyou/BCI_ROS/build/online_analysis
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
