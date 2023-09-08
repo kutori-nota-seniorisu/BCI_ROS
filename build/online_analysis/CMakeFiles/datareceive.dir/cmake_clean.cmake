8fbbe9b43888cbf95f989eb1c63dc26e0353e519
wuyou <1443207784@qq.com>
2023-09-08 21:02:31 +0800

    this is written by wy

---
+++ build/online_analysis/CMakeFiles/datareceive.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/datareceive.dir/src/datareceive.cpp.o"
+  "/home/sd/BCI_ROS/devel/lib/online_analysis/datareceive.pdb"
+  "/home/sd/BCI_ROS/devel/lib/online_analysis/datareceive"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/datareceive.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/online_analysis/CTestTestfile.cmake
@@ -0,0 +1,6 @@
+# CMake generated Testfile for 
+# Source directory: /home/sd/BCI_ROS/src/online_analysis
+# Build directory: /home/sd/BCI_ROS/build/online_analysis
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
