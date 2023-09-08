655fd8d915feefa2965a7e7835870a17ab2bc96e
wuyou <1443207784@qq.com>
2023-09-08 21:44:59 +0800

    删除了devel与build

--- build/key_read/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/sd/BCI_ROS/src/key_read
-# Build directory: /home/sd/BCI_ROS/build/key_read
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
